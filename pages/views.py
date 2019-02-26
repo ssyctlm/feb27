f493eab5338287c2e6d0234260f9a4ee695e2979
ssyctlm <myqy480@gmail.com>
2019-02-26 13:30:42 +0800

    today I learned how to use for loop and condition statements in django view rendering

--- pages/views.py
+++ pages/views.py
@@ -16,8 +16,16 @@ def about_view(request,*args, **kwargs):
     return render(request,'about.html')
 
 def contact_view(request,*args, **kwargs):
+    my_context = {
+        'address': "The north 3rd ring road",
+        'postcode':"100072",
+        'phonenumber':"010-63812023",
+        'isopen': True,
+        'contact_list':['liming',123,356,987]
+
+    }
     #return HttpResponse("<h1>This is contact page</h1>")
-    return render(request,'contact.html')
+    return render(request,'contact.html',my_context)
 
 def solution_view(request,*args, **kwargs):
     #return HttpResponse("<h1>This is solution page</h1>")
