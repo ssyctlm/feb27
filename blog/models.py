8c666cf584e1589d06be16ba8b1266cb27646f39
bunbun <none>
2019-03-19 21:41:07 +0800

    just done the detail page of blog articles as tutorial, figure it out another day.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title   = models.CharField(max_length = 60)
+    title   = models.CharField(max_length=60)
     content = models.TextField()
     active  = models.BooleanField(default = True)
